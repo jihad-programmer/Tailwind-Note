eb5aba69c191775d9096f9b5374caf4630e6a150
jihad-programmer <mdjaminur2300@gmail.com>
2024-07-14 02:49:49 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors: {
+      blue: {
+        '400' : '#e63946'
+      }
+    },
+    extend: {
+      "fontFamily": {
+        'open': ['Open Sans", sans-serif']
+      },
+      colors: {
+        'hablu' : '#e63946'
+      }
+    },
+  },
+  plugins: [],
+}
+
